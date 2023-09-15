35f05dae601b0df88c561ea57c794c4482db637c
Toru Ogawa <Hakuyume@users.noreply.github.com>
2023-09-15 13:01:46 +0900

    impl Service

--- src/client.rs
+++ src/client.rs
@@ -1,8 +1,12 @@
 use crate::Cache;
 use chrono::Utc;
 use k8s_openapi::api::authentication::v1::{TokenReview, TokenReviewSpec, TokenReviewStatus};
+use std::future::Future;
+use std::pin::Pin;
 use std::sync::Arc;
+use std::task::{Context, Poll};
 use tokio::sync::RwLock;
+use tower::Service;
 
 #[derive(Clone)]
 pub struct Client {
@@ -21,32 +25,60 @@ impl Client {
     pub async fn try_default() -> Result<Self, kube::Error> {
         Ok(Self::new(kube::Client::try_default().await?))
     }
+}
 
-    pub async fn call(
-        &self,
-        spec: TokenReviewSpec,
-    ) -> Result<Option<TokenReviewStatus>, kube::Error> {
-        if crate::exp(&spec) > Some(Utc::now().timestamp()) {
-            let status = self.cache.read().await.get(&spec).cloned();
-            if let Some(status) = status {
-                Ok(Some(status))
+impl Service<TokenReviewSpec> for &Client {
+    type Response = Option<TokenReviewStatus>;
+    type Error = kube::Error;
+    type Future =
+        Pin<Box<dyn Future<Output = Result<Self::Response, Self::Error>> + Send + 'static>>;
+
+    fn poll_ready(&mut self, _: &mut Context<'_>) -> Poll<Result<(), Self::Error>> {
+        Poll::Ready(Ok(()))
+    }
+
+    fn call(&mut self, request: TokenReviewSpec) -> Self::Future {
+        let client = self.client.clone();
+        let cache = self.cache.clone();
+        Box::pin(async move {
+            if crate::exp(&request) > Some(Utc::now().timestamp()) {
+                let status = cache.read().await.get(&request).cloned();
+                if let Some(status) = status {
+                    Ok(Some(status))
+                } else {
+                    let status = kube::Api::all(client)
+                        .create(
+                            &Default::default(),
+                            &TokenReview {
+                                spec: request.clone(),
+                                ..Default::default()
+                            },
+                        )
+                        .await?
+                        .status;
+                    if let Some(status) = &status {
+                        cache.write().await.put(request, status.clone());
+                    }
+                    Ok(status)
+                }
             } else {
-                let status = kube::Api::all(self.client.clone())
-                    .create(
-                        &Default::default(),
-                        &TokenReview {
-                            spec: spec.clone(),
-                            ..Default::default()
-                        },
-                    )
-                    .await?
-                    .status
-                    .unwrap();
-                self.cache.write().await.put(spec, status.clone());
-                Ok(Some(status))
+                Ok(None)
             }
-        } else {
-            Ok(None)
-        }
+        })
+    }
+}
+
+impl Service<TokenReviewSpec> for Client {
+    type Response = Option<TokenReviewStatus>;
+    type Error = kube::Error;
+    type Future =
+        Pin<Box<dyn Future<Output = Result<Self::Response, Self::Error>> + Send + 'static>>;
+
+    fn poll_ready(&mut self, cx: &mut Context<'_>) -> Poll<Result<(), Self::Error>> {
+        (&*self).poll_ready(cx)
+    }
+
+    fn call(&mut self, request: TokenReviewSpec) -> Self::Future {
+        (&*self).call(request)
     }
 }
